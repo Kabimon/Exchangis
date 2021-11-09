88181abdf34a4cf402e1ab758473d79e8fcd3f2d
Dlimeng <77954309@qq.com>
2021-11-09 22:13:24 +0800

    plugin

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/config/ExchangisConfig.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/config/ExchangisConfig.java
@@ -8,4 +8,14 @@ public class ExchangisConfig {
     public final static String BASEURL = "/api/rest_s/" + ServerConfiguration.BDP_SERVER_VERSION() + "/exchangis";
     public final static String EXCHANGIS_APPCONN_NAME = CommonVars.apply("wds.dss.appconn.exchangis.name", "Exchangis").getValue();
 
+    public final static String ID="id";
+    public final static String WORKSPACE_NAME="workspaceName";
+    public final static String PROJECT_NAME="projectName";
+    public final static String DESCRIPTION="description";
+    public final static String TAGS="tags";
+    public final static String EDIT_USERS="editUsers";
+    public final static String VIEW_USERS="viewUsers";
+    public final static String EXEC_USERS="execUsers";
+
+
 }
--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/project/ExchangisProjectUpdateOperation.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/project/ExchangisProjectUpdateOperation.java
@@ -1,18 +1,97 @@
 package com.webank.wedatasphere.exchangis.appconn.project;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Maps;
+import com.webank.wedatasphere.dss.common.label.DSSLabel;
+import com.webank.wedatasphere.dss.standard.app.sso.builder.SSOUrlBuilderOperation;
+import com.webank.wedatasphere.dss.standard.app.sso.request.SSORequestOperation;
 import com.webank.wedatasphere.dss.standard.app.structure.StructureService;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectRequestRef;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectResponseRef;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectUpdateOperation;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
+import com.webank.wedatasphere.exchangis.appconn.config.ExchangisConfig;
+import com.webank.wedatasphere.exchangis.appconn.model.ExchangisPutAction;
+import com.webank.wedatasphere.exchangis.appconn.ref.ExchangisProjectResponseRef;
+import com.webank.wedatasphere.linkis.httpclient.request.HttpAction;
+import com.webank.wedatasphere.linkis.httpclient.response.HttpResult;
+import com.webank.wedatasphere.linkis.manager.label.entity.SerializableLabel;
+import com.webank.wedatasphere.linkis.server.BDPJettyServerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ExchangisProjectUpdateOperation implements ProjectUpdateOperation {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExchangisProjectUpdateOperation.class);
+    private static final Logger logger = LoggerFactory.getLogger(ExchangisProjectUpdateOperation.class);
+    private SSORequestOperation<HttpAction, HttpResult> ssoRequestOperation;
+    private StructureService structureService;
+
+    public ExchangisProjectUpdateOperation(SSORequestOperation<HttpAction, HttpResult> ssoRequestOperation, StructureService structureService) {
+        this.ssoRequestOperation = ssoRequestOperation;
+        this.structureService = structureService;
+    }
+
+    private String getAppName() {
+        return ExchangisConfig.EXCHANGIS_APPCONN_NAME;
+    }
     @Override
     public ProjectResponseRef updateProject(ProjectRequestRef projectRequestRef) throws ExternalOperationFailedException {
-        return null;
+        String url=ExchangisConfig.BASEURL+"/updateProject";
+
+        List<DSSLabel> dssLabels = projectRequestRef.getDSSLabels();
+        String dssLabelStr="";
+        if(dssLabels != null && !dssLabels.isEmpty()){
+            dssLabelStr=dssLabels.stream().map(SerializableLabel::getStringValue).collect(Collectors.joining(","));
+        }
+        ExchangisPutAction exchangisPutAction = new ExchangisPutAction();
+        exchangisPutAction.setUser(projectRequestRef.getCreateBy());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.ID,projectRequestRef.getId());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.WORKSPACE_NAME,projectRequestRef.getWorkspaceName());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.PROJECT_NAME,projectRequestRef.getName());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.DESCRIPTION,projectRequestRef.getDescription());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.EDIT_USERS,projectRequestRef.getUpdateBy());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.EXEC_USERS,projectRequestRef.getUpdateBy());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.VIEW_USERS,projectRequestRef.getUpdateBy());
+        exchangisPutAction.addRequestPayload(ExchangisConfig.TAGS,dssLabelStr);
+
+        SSOUrlBuilderOperation ssoUrlBuilderOperation = projectRequestRef.getWorkspace().getSSOUrlBuilderOperation().copy();
+        ssoUrlBuilderOperation.setAppName(getAppName());
+        ssoUrlBuilderOperation.setReqUrl(url);
+        ssoUrlBuilderOperation.setWorkspace(projectRequestRef.getWorkspace().getWorkspaceName());
+
+        String response = "";
+        Map<String, Object> resMap = Maps.newHashMap();
+        HttpResult httpResult = null;
+        try {
+            exchangisPutAction.setUrl(ssoUrlBuilderOperation.getBuiltUrl());
+            httpResult = this.ssoRequestOperation.requestWithSSO(ssoUrlBuilderOperation, exchangisPutAction);
+            response = httpResult.getResponseBody();
+            resMap = BDPJettyServerHelper.jacksonJson().readValue(response, new TypeReference<Map<String, Object>>() {});
+        }catch (Exception e){
+            logger.error("Create Exchangis Project Exception", e);
+            throw new ExternalOperationFailedException(31020,e.getMessage());
+        }
+        Map<String, Object> header = (Map<String, Object>) resMap.get("header");
+        int code = (int) header.get("code");
+        String errorMsg = "";
+        if (code != 200) {
+            errorMsg = header.toString();
+            throw new ExternalOperationFailedException(31020, errorMsg, null);
+        }
+        Integer projectId = (Integer) ((Map<String, Object>) resMap.get("payload")).get("projectId");
+        ExchangisProjectResponseRef exchangisProjectResponseRef = null;
+        try {
+            exchangisProjectResponseRef = new ExchangisProjectResponseRef(response, code);
+        } catch (Exception e) {
+            throw new ExternalOperationFailedException(31020, "failed to parse response json", e);
+        }
+        exchangisProjectResponseRef.setAppInstance(structureService.getAppInstance());
+        exchangisProjectResponseRef.setProjectRefId(projectId.longValue());
+        exchangisProjectResponseRef.setErrorMsg(errorMsg);
+        return exchangisProjectResponseRef;
     }
 
     @Override
@@ -22,6 +101,6 @@ public class ExchangisProjectUpdateOperation implements ProjectUpdateOperation {
 
     @Override
     public void setStructureService(StructureService structureService) {
-
+        this.structureService=structureService;
     }
 }
